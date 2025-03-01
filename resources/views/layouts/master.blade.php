7aab00326266a69f93a40b34bac755297a1952e8
alimusyafa-psc <alimusyafa.2711@gmail.com>
2025-03-01 23:02:57 +0700

    logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,7 +43,7 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0">
-        <img src="{{ asset('img/PENS.png') }}" class="me-3" alt="xd" style="width: 60px; height: 100px;">
+        <img src="{{ asset('img/PENS.png') }}" class="me-3" alt="xd">
         <span class="ms-1 font-weight-bold">Log Aktivitas</span>
       </a>
     </div>
