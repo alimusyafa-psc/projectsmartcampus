21fc982e6dd24db0039949dd97c7b559bb22ae7f
Endryco Farel Rianrachmatullah <88662707+endrycofr@users.noreply.github.com>
2025-03-07 22:53:11 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e  # Hentikan eksekusi jika ada error
+
+# Set permission untuk storage dan bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Pastikan storage/logs ada dan memiliki izin yang benar
+mkdir -p /var/www/html/storage/logs
+touch /var/www/html/storage/logs/laravel.log
+chown -R www-data:www-data /var/www/html/storage/logs
+chmod -R 775 /var/www/html/storage/logs
+chmod 664 /var/www/html/storage/logs/laravel.log
+
+# Jalankan perintah utama container
+exec "$@"
