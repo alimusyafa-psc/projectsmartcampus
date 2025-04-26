8c51dcf057c84c337d10c829bfd999180501fbbe
Endryco Farel Rianrachmatullah <88662707+endrycofr@users.noreply.github.com>
2025-04-27 00:58:32 +0700

    conf nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,8 @@ touch /var/www/html/storage/logs/laravel.log
 chown -R www-data:www-data /var/www/html/storage/logs
 chmod -R 775 /var/www/html/storage/logs
 chmod 664 /var/www/html/storage/logs/laravel.log
+chmod -R 755 /var/www/html/public/assets/
+ chown -R www-data:www-data /var/www/html/public/assets/
 
 # Jalankan perintah utama container
 exec "$@"
