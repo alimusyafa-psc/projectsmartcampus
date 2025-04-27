500dab42be99ebb0778b2c204b0f7036b4f43c7d
Endryco Farel Rianrachmatullah <88662707+endrycofr@users.noreply.github.com>
2025-04-27 10:24:25 +0700

    entrypoint permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,22 @@ set -e  # Hentikan eksekusi jika ada error
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Pastikan storage/logs ada dan memiliki izin yang benar
+# Pastikan storage/logs ada
 mkdir -p /var/www/html/storage/logs
 touch /var/www/html/storage/logs/laravel.log
 chown -R www-data:www-data /var/www/html/storage/logs
 chmod -R 775 /var/www/html/storage/logs
 chmod 664 /var/www/html/storage/logs/laravel.log
-chmod -R 755 /var/www/html/public/assets/
-chown -R www-data:www-data /var/www/html/public/assets/
 
+# ✅ Pastikan public/assets ada
+mkdir -p /var/www/html/public/assets
+chown -R www-data:www-data /var/www/html/public/assets
+chmod -R 755 /var/www/html/public/assets
 
 # Cache config and routes (recommended in production)
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
 # Jalankan perintah utama container
 exec "$@"
