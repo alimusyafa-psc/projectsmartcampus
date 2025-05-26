8b42c8ad1b2a4fc8e497face9f13eed31fce5734
Endryco Farel Rianrachmatullah <88662707+endrycofr@users.noreply.github.com>
2025-05-26 13:14:08 +0700

    menambahkan

--- app/Models/Datamahasiswa.php
+++ app/Models/Datamahasiswa.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Relay;
 class Datamahasiswa extends Model
 {
     use HasFactory;
